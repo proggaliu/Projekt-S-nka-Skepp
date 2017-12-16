0f20c3f3e76fba97713e813519352910a8210a78
Jonas <menvafan123456789@hotmail.se>
2017-12-16 17:23:56 +0100

    Nya metoder, snyggare kod

--- sankaSkepp/src/sankaSkepp/Spelplan.java
+++ sankaSkepp/src/sankaSkepp/Spelplan.java
@@ -14,15 +14,28 @@ public class Spelplan {
 		int [][] nySpelplan = new int[kolumn][rad];
 
 
-
+//Fyller spelplanen med vatten
 		for(i = 0; i < rad; i++){
 			for(j = 0; j < kolumn; j++){
 				nySpelplan[i][j] = -1;
 			}
 		}
+	}
+	//Använder hämtar koordinater från tangentbordet
+	public static String nyaKoordinater() {
+		String nyKoordinat = "";
+		
+		do {Scanner scanner = new Scanner(System.in);
+		nyKoordinat = scanner.toString();} while (nyKoordinat.length() == 2);
+				
+		return nyKoordinat;
+	}
 
-
-		Scanner scanner = new Scanner(System.in);
+	
+	//Metoden för att lägga ut båtar på spelplaen
+	public static int [][] placeraFartyg (int [][] nySpelplan) {
+	
+		
 		System.out.println("På vilken rad vill du placera din båt?");
 		i = scanner.nextInt()-1;
 		System.out.println("På vilken kolumn vill du placera din båt?");
@@ -30,9 +43,25 @@ public class Spelplan {
 
 		nySpelplan[i][j] = 5;
 
+return nySpelplan;
 
+	}
+		//Prova att skjuta
+	public static int [][] skjut (int [][] nySpelplan) {
+		boolean result = false;
 
-		//Visa spelplanen(bör göras till en metod)
+		System.out.println("På vilken rad vill du skjuta?");
+		i = scanner.nextInt()-1;
+		System.out.println("På vilken kolumn vill du skjuta?");
+		j = scanner.nextInt()-1;
+
+		if(nySpelplan[i][j] == 5) { System.out.println("Träff"); nySpelplan[i][j] = 10;}
+		else if (nySpelplan[i][j] == -1) {System.out.println("Miss"); nySpelplan[i][j] = 7;}
+	}
+
+	
+//Metoden som används för att printa spelplanen
+	public static void visaPlanen (int [][] nySpelplan) {
 		System.out.print("  A B C D E F G H I");
 		for(int r =0; r < 9; r++)	
 		{ 	
@@ -40,72 +69,28 @@ public class Spelplan {
 			System.out.print((r + 1) + "");
 			for(int c = 0; c < 9; c++)
 			{
-				
+
 				if(nySpelplan[r][c] == -1)
 				{
-					System.out.print(" ~");
+					System.out.print(" ~");//vatten
 				}
 				else if (nySpelplan[r][c] == 5)
 				{
-					System.out.print(" x");
+					System.out.print(" x");//båt
 				}
-				
+
 				else if (nySpelplan[r][c] == 10)
 				{
-					System.out.print(" t");
+					System.out.print(" t");//träff
 				}
 				else if (nySpelplan[r][c] == 7)
 				{
-					System.out.print(" m");
+					System.out.print(" m");//miss
 				}
 			}
 		}
-System.out.println("");
-		//Prova att skjuta
-
-		boolean result = false;
-		
-		System.out.println("På vilken rad vill du skjuta?");
-		i = scanner.nextInt()-1;
-		System.out.println("På vilken kolumn vill du skjuta?");
-		j = scanner.nextInt()-1;
-		
-		if(nySpelplan[i][j] == 5) { System.out.println("Träff"); nySpelplan[i][j] = 10;}
-		else if (nySpelplan[i][j] == -1) {System.out.println("Miss"); nySpelplan[i][j] = 7;}
-		
-		
-		//Visa spelplanen(bör göras till en metod)
-				System.out.print("  A B C D E F G H I");
-				for(int r =0; r < 9; r++)	
-				{ 	
-					System.out.println("");
-					System.out.print((r + 1) + "");
-					for(int c = 0; c < 9; c++)
-					{
-						
-						if(nySpelplan[r][c] == -1)
-						{
-							System.out.print(" ~");
-						}
-						else if (nySpelplan[r][c] == 5)
-						{
-							System.out.print(" x");
-						}
-						
-						else if (nySpelplan[r][c] == 10)
-						{
-							System.out.print(" t");
-						}
-						else if (nySpelplan[r][c] == 7)
-						{
-							System.out.print(" m");
-						}
-					}
-				}
-		
+		System.out.println("");
 	}
-
-
 }
 
 
